1ee1a7203a3d03447203e16a9605a7808a2376e1
Blad3Mak3r <blademaker.live@gmail.com>
2021-02-23 10:12:58 +0100

    Prepare project

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.30"
+}
+
+group = "com.github.blad3mak3r"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("org.json:json:20201115")
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.useIR = true
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
